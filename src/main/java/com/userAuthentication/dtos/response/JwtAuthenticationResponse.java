5472071e86d1ee1d05b074e7c303ade4eab5ae50
Rian <riansantosjr11@gmail.com>
2024-03-07 15:55:19 -0300

    feat: add response dto

---
+++ src/main/java/com/userAuthentication/dtos/response/JwtAuthenticationResponse.java
@@ -0,0 +1,15 @@
+package com.userAuthentication.dtos.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class JwtAuthenticationResponse {
+
+    private String token;
+}
