edca63bdae85e8b487b6cbfd30396503c0e46fd1
Rian <riansantosjr11@gmail.com>
2024-03-07 15:55:05 -0300

    feat: add request dto

---
+++ src/main/java/com/userAuthentication/dtos/request/SignUpRequestDto.java
@@ -0,0 +1,18 @@
+package com.userAuthentication.dtos.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class SignUpRequestDto {
+
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+}
