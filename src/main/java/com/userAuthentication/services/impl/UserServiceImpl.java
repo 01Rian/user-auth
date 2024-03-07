323a9df77109dd94d2a29737b26583c9441af908
Rian <riansantosjr11@gmail.com>
2024-03-07 15:58:05 -0300

    feat: add services implementation

---
+++ src/main/java/com/userAuthentication/services/impl/UserServiceImpl.java
@@ -0,0 +1,27 @@
+package com.userAuthentication.services.impl;
+
+import com.userAuthentication.repository.UserRepository;
+import com.userAuthentication.services.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class UserServiceImpl implements UserService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetailsService userDetailsService() {
+        return new UserDetailsService() {
+            @Override
+            public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+                return userRepository.findByEmail(username)
+                        .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+            }
+        };
+    }
+}
