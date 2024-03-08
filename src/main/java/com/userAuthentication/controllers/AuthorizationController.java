7a8bcfec1eb9cd2753ade68b2df0e3d91ee1a377
Rian <riansantosjr11@gmail.com>
2024-03-08 18:47:54 -0300

    feat: add endpoint findByEmail

--- src/main/java/com/userAuthentication/controllers/AuthorizationController.java
+++ src/main/java/com/userAuthentication/controllers/AuthorizationController.java
@@ -1,18 +1,20 @@
 package com.userAuthentication.controllers;
 
+import com.userAuthentication.dtos.response.UserDto;
+import com.userAuthentication.services.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/api/v1/resource")
 public class AuthorizationController {
 
+    private final UserService userService;
+
     @GetMapping
-    public ResponseEntity<String> sayHello() {
-        return ResponseEntity.ok("Here is your resource");
+    public ResponseEntity<UserDto> findByEmail(@RequestBody UserDto userDto) {
+        return ResponseEntity.ok(userService.findByEmail(userDto.getEmail()));
     }
 }
