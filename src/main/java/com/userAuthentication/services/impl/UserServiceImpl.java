d80e4f6215066a0a6826e0db8bcedcb9e067eea9
Rian <riansantosjr11@gmail.com>
2024-04-16 17:20:40 -0300

    refactor: move UserDetailsService to another configuration class

--- src/main/java/com/userAuthentication/services/impl/UserServiceImpl.java
+++ src/main/java/com/userAuthentication/services/impl/UserServiceImpl.java
@@ -8,9 +8,6 @@ import com.userAuthentication.services.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -24,17 +21,6 @@ public class UserServiceImpl implements UserService {
 
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
-
-    @Override
-    public UserDetailsService userDetailsService() {
-        return new UserDetailsService() {
-            @Override
-            public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-                return userRepository.findByEmail(username)
-                        .orElseThrow(() -> new UsernameNotFoundException("User not found"));
-            }
-        };
-    }
 
     @Override
     @Transactional(readOnly = true)
