7afb0f4610620ad4a6d32eeb39afff19fe768da4
Rian <riansantosjr11@gmail.com>
2024-04-16 17:22:59 -0300

    feat: adds implementations to CRUD operations, and define PreAuthorize

--- src/main/java/com/userAuthentication/controllers/AuthorizationController.java
+++ src/main/java/com/userAuthentication/controllers/AuthorizationController.java
@@ -1,10 +1,14 @@
 package com.userAuthentication.controllers;
 
+import com.userAuthentication.dtos.request.SignUpRequestDto;
 import com.userAuthentication.dtos.response.UserDtoResponse;
 import com.userAuthentication.services.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Collection;
 
 @RequiredArgsConstructor
 @RestController
@@ -13,8 +17,38 @@ public class AuthorizationController {
 
     private final UserService userService;
 
+    @GetMapping("/me")
+    public ResponseEntity<UserDtoResponse> authenticatedUser() {
+        var user = userService.authenticatedUser();
+
+        return ResponseEntity.ok(user);
+    }
+
+    @PreAuthorize("hasAuthority('ADMIN')")
     @GetMapping
+    public ResponseEntity<Collection<UserDtoResponse>> findAll() {
+        Collection<UserDtoResponse> users = userService.findAll();
+        return ResponseEntity.ok(users);
+    }
+
+    @PreAuthorize("hasAuthority('ADMIN')")
+    @GetMapping("/email")
     public ResponseEntity<UserDtoResponse> findByEmail(@RequestBody UserDtoResponse userDtoResponse) {
-        return ResponseEntity.ok(userService.findByEmail(userDtoResponse.email()));
+        var user = userService.findByEmail(userDtoResponse.email());
+        return ResponseEntity.ok(user);
+    }
+
+    @PreAuthorize("hasAuthority('ADMIN')")
+    @PutMapping("/{id}")
+    public ResponseEntity<UserDtoResponse> updatedUserAuth(@RequestBody SignUpRequestDto dto, @PathVariable Long id) {
+        UserDtoResponse updatedUser = userService.updateUser(dto, id);
+        return ResponseEntity.ok(updatedUser);
+    }
+
+    @PreAuthorize("hasAuthority('ADMIN')")
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteById(@PathVariable Long id) {
+        userService.deleteById(id);
+        return ResponseEntity.noContent().build();
     }
 }
